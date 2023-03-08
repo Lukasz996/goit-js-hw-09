23ec7d67147dcd9b4bc25e3945c00671286a14e0
Łukasz <lukaszkogut1996@gmail.com>
2023-03-08 21:59:26 +0100

    html change/script

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
-const button = document.querySelector("button");
+// const startBtn = document.querySelector("button");
+// const stopBtn = document.querySelector("button").nextElementSibling;
+// let timerId = null;
+
+// function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+//   }
+
+// startBtn.addEventListener("click", () => {
+//     timerId = setinterval(() => {
+//       const body = document.querySelector('body')
+//       body.style.background = getRandomHexColor()
+//     }, 1000);
+// });
+
+// stopBtn.addEventListener('click', () => {
+//   clearInterval(timerId);
+// });
+
+
+
+const startBtn = document.querySelector('button');
+const stopBtn = document.querySelector('button').nextElementSibling;
+let timerId = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+startBtn.addEventListener('click', () => {
+ timerId = setInterval(() => {
+    const body = document.querySelector('body')
+    body.style.background = getRandomHexColor()
+  }, 1000);
+});
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+});
